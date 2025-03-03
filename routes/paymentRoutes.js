5d40776c105c6bd46ba13be7f502d87b4f762f6b
anand <anand.krishna5154@gmail.com>
2025-03-03 21:45:02 +0530

    Remove all files

--- routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getPaymentDetails, sendContactMessage } from '../controllers/paymentController.js';
-
-const router = express.Router();
-
-// ... existing routes ...
-router.post('/contact', sendContactMessage);
-
-export default router; 
